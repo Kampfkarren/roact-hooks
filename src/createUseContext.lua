32798ae45b32c7d5ba0b4233fb72f3c6732ad751
Lucas Gangstad <last_talon@new.rr.com>
2022-10-29 10:58:27 -0500

    Update useContext to provide the default value

--- src/createUseContext.lua
+++ src/createUseContext.lua
@@ -7,20 +7,32 @@ local function createUseContext(component, useEffect, useState)
 	})
 
 	return function(context)
+		local defaultValue
+
+		fakeConsumer.props = {
+			render = function(value)
+				defaultValue = value
+			end,
+		}
+
 		context.Consumer.init(fakeConsumer)
+		context.Consumer.render(fakeConsumer)
 
 		local contextEntry = fakeConsumer.contextEntry
-		local value, setValue = useState(if contextEntry == nil then nil else contextEntry.value)
+		local value, setValue = useState(if contextEntry == nil then defaultValue else contextEntry.value)
 
 		useEffect(function()
 			if contextEntry == nil then
+				if value ~= defaultValue then
+					setValue(defaultValue)
+				end
 				return
 			end
 
 			if value ~= contextEntry.value then
 				setValue(contextEntry.value)
 			end
-			
+
 			return contextEntry.onUpdate:subscribe(setValue)
 		end, { contextEntry })
 
